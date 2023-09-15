c6b740f9dd3f4530d6ffac361bd9ff827b87aa48
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2023-09-15 21:35:42 +0530

    FIX #2 Add support for more Indian languages Languages Supported: English, Hindi, Kannada, Tamil, Telugu, Malayalam

--- Backend/image_crawler.py
+++ Backend/image_crawler.py
@@ -1,15 +1,15 @@
-import pytesseract
 import os
 
+import pytesseract
 from PIL import Image
 
 def get_ocr(file:str, lang:str = 'eng') -> tuple:
     print(f"Running OCR for {file} ...")
-    
-    img = Image.open(file)
+
     os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    
+
+    img = Image.open(file)
     text = pytesseract.image_to_string(img, lang=lang)
 
     print("Done ...")
--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,18 @@
 import asyncio
+import json
 
 from text_crawler import scrape_ndtv_archive
 from image_crawler import get_ocr
 from translator import get_translation
+from json_saver import save_to_json
+
+outlets_dict = {
+    'Hindustan': ['Hindi', 'hin'],
+    'Vijayavani': ['Kannada', 'kan'],
+    'Dinamalar': ['Tamil', 'tam'],
+    'Mathrubhumi': ['Malayalam', 'mal'],
+    'Sakshi': ['Telugu', 'tel']
+}
 
 async def main():
     news_outlet = input("News Outlet: ")
@@ -13,11 +23,18 @@ async def main():
         max_articles_to_scrape = 1000
 
         await scrape_ndtv_archive(news_outlet, target_date, source_url, max_articles_to_scrape)
-    elif news_outlet.lower() == "hindustan dainik":
-        file_path = r'Backend\Assets\hindi-1.jpg'
+        
+    elif news_outlet in outlets_dict:
+        language = outlets_dict[news_outlet][0]
+        file_path = rf'Backend\Assets\{outlets_dict[news_outlet][1]}-1.jpg'
+        
+        with open(r'Backend\language_codes.json', 'r', encoding='utf-8') as f:
+            lang_codes = json.load(f)
             
-        ocr_data = await asyncio.to_thread(get_ocr, file_path, lang = "hin")
-        translator_data = await asyncio.to_thread(get_translation, ocr_data, lang = "hi")
+        ocr_data = get_ocr(file_path, lang=lang_codes[language][1])
+        translator_data = get_translation(ocr_data, lang=lang_codes[language][0])
+        save_to_json(r'Backend\translated.json', translator_data)
+        
     else:
         print("\n[Error] News outlet not supported")
 
