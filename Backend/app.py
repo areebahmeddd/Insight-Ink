ece7abdbc57b10f04ec1081bf49bdfcb06fe5a1e
Areeb Ahmed <areebahmed0709@gmail.com>
2023-10-15 03:18:36 +0530

    🕸️ Added API endpoints for Frontend

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,18 @@
 import asyncio
 import json
 
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from text_crawler import scrape_ndtv_archive
 from image_crawler import perform_ocr
 from google_translator import perform_translation
 
-app = Flask(__name__, template_folder = "../Frontend")
+app = Flask(__name__)
+CORS(app, resources = {
+        r'/api/*': {"origins": ["http://localhost:3000", "https://insight-ink.org"]}
+    }
+)
 
 media_outlets = {
     "Dainik Jagran": ["Hindi", "hin"],
@@ -17,43 +22,40 @@ media_outlets = {
     "Eenadu": ["Telugu", "tel"]
 }
 
-@app.route("/", methods = ["GET", "POST"])
-def home() -> str:
-    if request.method == "POST":
-        news_source = request.form["news_source"]
-        target_date = request.form["target_date"]
+@app.route("/api", methods = ["POST"])
+def process_data() -> str:
+    news_source = request.json.get("news_source")
+    target_date = request.json.get("target_date")
 
-        if news_source == "NDTV":
-            source_url = f'https://archives.ndtv.com/articles/{target_date}.html'
-            max_articles_to_scrape = 100
+    if news_source == "NDTV":
+        source_url = f'https://archives.ndtv.com/articles/{target_date}.html'
+        max_articles_to_scrape = 500
 
-            asyncio.run(scrape_ndtv_archive(news_source, target_date, source_url, max_articles_to_scrape))
+        asyncio.run(scrape_ndtv_archive(news_source, target_date, source_url, max_articles_to_scrape))
 
-            with open(r'Backend\english.json', "r", encoding = "utf-8") as json_file:
-                scraped_data = json.load(json_file)
+        with open(r'Frontend\src\english.json', "r", encoding = "utf-8") as json_file:
+            scraped_data = json.load(json_file)
 
-            return render_template("test.html", json_data = json.dumps(scraped_data, indent = 4))
+        return jsonify(scraped_data)
 
-        elif news_source in media_outlets:
-            source_language = media_outlets[news_source][0]
-            image_file_path = rf'Backend\Assets\{media_outlets[news_source][1]}-1.jpg'
+    elif news_source in media_outlets:
+        source_language = media_outlets[news_source][0]
+        image_file_path = rf'Backend\Assets\{media_outlets[news_source][1]}-1.jpg'
 
-            with open(r'Backend\language_codes.json', "r", encoding = "utf-8") as json_file:
-                language_mappings = json.load(json_file)
+        with open(r'Backend\language_codes.json', "r", encoding = "utf-8") as json_file:
+            language_mappings = json.load(json_file)
 
-            extracted_text = perform_ocr(image_file_path, language_mappings[source_language][1])
-            json_file_path = rf'Backend\{source_language.lower()}.json'
-            perform_translation(news_source, extracted_text, json_file_path, language_mappings[source_language][0])
+        extracted_text = perform_ocr(image_file_path, language_mappings[source_language][1])
+        json_file_path = rf'Frontend\src\{source_language.lower()}.json'
+        perform_translation(news_source, extracted_text, json_file_path, language_mappings[source_language][0])
 
-            with open(json_file_path, "r", encoding = "utf-8") as json_file:
-                scraped_data = json.load(json_file)
+        with open(json_file_path, "r", encoding = "utf-8") as json_file:
+            scraped_data = json.load(json_file)
 
-            return render_template("test.html", json_data = json.dumps(scraped_data, indent = 4))
+        return jsonify(scraped_data)
 
-        else:
-            return "News source not supported"
     else:
-        return render_template("test.html")
+        return jsonify({"Error": "News source not supported"})
 
 if __name__ == "__main__":
     app.run()
